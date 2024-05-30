83325fe8bb0836e090e49e9ab44131618dc6aadd
SzymonWichrowski <szymonwichrowski99@gmail.com>
2024-05-30 21:29:46 +0200

    firebase init

---
+++ tranquil-travels/src/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCGq7w6Ngm7bIBOtdB0bXXur4upVbHX4fw",
+  authDomain: "tranquil-travels-422ccc.firebaseapp.com",
+  projectId: "tranquil-travels-422ccc",
+  storageBucket: "tranquil-travels-422ccc.appspot.com",
+  messagingSenderId: "438275805622",
+  appId: "1:438275805622:web:07c6d7033fb39774fcf117"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
